60ef29ccf50455d4c0cd3d01feebc7df9b30e544
Sebastien Lenard <sebastien.lenard@gmail.com>
2024-10-23 23:34:57 -0600

    loadVariable: adaptation to new daily spires file for new regions.

--- tbx/ESPToolbox/loadVariableForSpiresSmooth20240204.m
+++ tbx/ESPToolbox/loadVariableForSpiresSmooth20240204.m
@@ -1,5 +1,6 @@
 % Seb 20240204 Additional functions to load variables and save the output var by var to lower consumption.
-function out = loadVariableForSpiresSmooth20240204(j, firstDateOfMonthForSmoothing, region, vars, divisor, dtype, matdates, out, cellIdx)
+function out = loadVariableForSpiresSmooth20240204(j, firstDateOfMonthForSmoothing, region, vars, divisor, dtype, matdates, out, cellIdx, waterYearDate, varInSpiresDaily)
+    espEnv = region.espEnv;
     scratchPath = region.espEnv.scratchPath;
     regionName = region.name;
     fprintf('Loading variable %d...\n', j);
@@ -13,34 +14,71 @@ function out = loadVariableForSpiresSmooth20240204(j, firstDateOfMonthForSmoothi
     columnStartId = uint32(countOfPixels / sqrt(countOfCells) * (floor((cellIdx - 1) / sqrt(countOfCells))) + 1);
     columnEndId = uint32(columnStartId + countOfPixels / sqrt(countOfCells) - 1);
     
-    % 2023-07-10. change dv into firstDateOfMonthForSmoothing and simplify use of date functions.
-    for i=1:length(firstDateOfMonthForSmoothing)
-        fprintf('Handling wateryear month %d...\n', i);
-        inloc = [scratchPath, 'modis/intermediary/spiresfill_', ...
-            region.espEnv.modisData.versionOf.modisspiresfill, ...
-            '/v006/', regionName, '/', char(firstDateOfMonthForSmoothing(i), 'yyyy'), '/']; 
-            % Seb 20240204 location of input distinct from output.
-        fname=fullfile(inloc,[regionName, '_', char(firstDateOfMonthForSmoothing(i), 'yyyyMM'), ...
-            '_', num2str(rowStartId), '_', num2str(rowEndId), '_', num2str(columnStartId), '_', num2str(columnEndId), '.mat']);
-        m=matfile(fname);
+    % Switch to spiresdaily for regions outside of westernUS. 20240916.
+    if ismember(regionName, {'h08v04', 'h08v05', 'h09v04', 'h09v05', 'h10v04'})
+      % 2023-07-10. change dv into firstDateOfMonthForSmoothing and simplify use of date functions.
+      for i=1:length(firstDateOfMonthForSmoothing)
+          fprintf('Handling wateryear month %d...\n', i);
+          inloc = [scratchPath, 'modis/intermediary/spiresfill_', ...
+              region.espEnv.modisData.versionOf.modisspiresfill, ...
+              '/v006/', regionName, '/', char(firstDateOfMonthForSmoothing(i), 'yyyy'), '/']; 
+              % Seb 20240204 location of input distinct from output.
+          fname=fullfile(inloc,[regionName, '_', char(firstDateOfMonthForSmoothing(i), 'yyyyMM'), ...
+              '_', num2str(rowStartId), '_', num2str(rowEndId), '_', num2str(columnStartId), '_', num2str(columnEndId), '.mat']);
+          m=matfile(fname);
 
-        if i==1
-            %for j=1:length(vars)
-            out.(vars{j})=zeros([size(m.(vars{j}),1) size(m.(vars{j}),2) ...
-                length(matdates)],'single');
-            %end
-        end
-        doy_start = daysdif(firstDateOfMonthForSmoothing(1), firstDateOfMonthForSmoothing(i)) + 1;
-        doy_end = doy_start + size(m.fsca, 3) - 1;
+          if i==1
+              %for j=1:length(vars)
+              out.(vars{j})=zeros([size(m.(vars{j}),1) size(m.(vars{j}),2) ...
+                  length(matdates)],'single');
+              %end
+          end
+          doy_start = daysdif(firstDateOfMonthForSmoothing(1), firstDateOfMonthForSmoothing(i)) + 1;
+          doy_end = doy_start + size(m.fsca, 3) - 1;
 
-        %convert to single and scale
-        %for j=1:length(vars)
-        tt=m.(vars{j})==intmax(dtype{j});
-        v=single(m.(vars{j}));
-        v(tt)=NaN;
-        v=v/divisor(j);
-        out.(vars{j})(:,:,doy_start:doy_end)=v;
-        %end
+          %convert to single and scale
+          %for j=1:length(vars)
+          tt=m.(vars{j})==intmax(dtype{j});
+          v=single(m.(vars{j}));
+          v(tt)=NaN;
+          v=v/divisor(j);
+          out.(vars{j})(:,:,doy_start:doy_end)=v;
+          %end
+      end
+    else
+      % For any other region, we use modisspiresdaily and
+      % espEnv.getDataForWaterYearDateAndVarName(). 20240916.
+      force = struct(type = 'single');
+      optim = struct();
+      optim.countOfCellPerDimension = [sqrt(countOfCells), sqrt(countOfCells)];
+      optim.cellIdx(1) = cellIdx - floor((cellIdx - 1) / optim.countOfCellPerDimension(1)) ...
+          * optim.countOfCellPerDimension(1);
+      optim.cellIdx(2) = floor((cellIdx - 1) / optim.countOfCellPerDimension(1)) + 1;
+      
+      thisIndex = sub2ind(optim.countOfCellPerDimension, ...
+        optim.cellIdx(1), optim.cellIdx(2));
+      objectName = regionName;
+      inputDataLabel = 'modspiresdaily';
+      varName = varInSpiresDaily{j}; % varName in modisspiresdaily. 20240916.
+      switch varName
+          case 'time_interp_weight_s' % Weights.
+              % force.divisor = 0.01;
+          case 'sensor_zenith'
+              force.resamplingFactor = 1;
+              force.resamplingMethod = 'nearest';
+          case 'solar_zenith'
+              force.resamplingFactor = 1;
+              force.resamplingMethod = 'nearest';
+          case 'solar_azimuth'
+              force.resamplingFactor = 1;
+              force.resamplingMethod = 'nearest';
+      end
+      varData = espEnv.getDataForWaterYearDateAndVarName( ...
+        objectName, inputDataLabel, waterYearDate, varName, force = force, ...
+        optim = optim);
+      varData = varData / divisor(j);
+      out.(vars{j}) = varData; % vars{j} varName expected by smoothSPIREScube script. 20240916.
+
     end
     fprintf('Loaded variable %d in %f secs.\n', j, toc);
 end
