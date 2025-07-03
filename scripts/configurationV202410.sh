6f0bf4c7bcfd711a562b45be1d50edf492f694ac
Sebastien Lenard <sebastien.lenard@gmail.com>
2025-07-03 11:36:55 -0600

    configuration, configurationV: 7ca0196438a86117c1a50c695152f56f112ab722 e4da0c32ddbdd21e6b8d6892764b1f9ee7a41a01 add configuration for run of historics. creation for V2024 and V2025, inverse order ftpExpor/webExpSn, change versionOfAncillary for pipeline 3.

--- scripts/configurationV202410.sh
+++ scripts/configurationV202410.sh
@@ -4,6 +4,8 @@
 
 # Script core.
 ########################################################################################
+source env/.matlabEnvironmentVariablesV202410
+
 # Configuration of scriptIds associated to script relative filePaths.
 ########################################################################################
 # scriptIds=(mod09gaI spiFillC spiSmooC moSpires scdInCub daMosaic snoStep3 webExpSn)
@@ -35,6 +37,40 @@ thoseVersionsOfAncillary[364]=v3.1
 thoseVersionsOfAncillary[5]=v3.1
 
 ########################################################################################
+# Month/monthwindow configuration for generation of historics.
+########################################################################################
+
+declare -A monthsForConfOfMonths
+monthsForConfOfMonths[10]="3 6 9 12"
+monthsForConfOfMonths[11]="1 2 3 4 5 6 7 8 9 10 11 12"
+monthsForConfOfMonths[20]="12"
+monthsForConfOfMonths[21]="10 11 12"
+monthsForConfOfMonths[30]="3 6 9"
+monthsForConfOfMonths[31]="1 2 3 4 5 6 7 8 9"
+monthsForConfOfMonths[41]="6 7 8 9"
+monthsForConfOfMonths[51]="9"
+monthsForConfOfMonths[120]="3"
+monthsForConfOfMonths[121]="1 2 3"
+monthsForConfOfMonths[130]="6 9 12"
+monthsForConfOfMonths[131]="4 5 6 7 8 9 10 11 12"
+monthsForConfOfMonths[141]="12"
+
+declare -A monthWindowsForConfOfMonths
+monthWindowsForConfOfMonths[10]=3
+monthWindowsForConfOfMonths[11]=1
+monthWindowsForConfOfMonths[20]=3
+monthWindowsForConfOfMonths[21]=1
+monthWindowsForConfOfMonths[30]=3
+monthWindowsForConfOfMonths[31]=1
+monthWindowsForConfOfMonths[41]=1
+monthWindowsForConfOfMonths[51]=12
+monthWindowsForConfOfMonths[120]=3
+monthWindowsForConfOfMonths[121]=1
+monthWindowsForConfOfMonths[130]=3
+monthWindowsForConfOfMonths[131]=1
+monthWindowsForConfOfMonths[141]=1
+
+########################################################################################
 # Configuration of pipelines with succession of scripts and versions of file data.
 ########################################################################################
 # Implemented like this because not possible to put arrays in values of a dictionary.
@@ -42,10 +78,14 @@ thoseVersionsOfAncillary[5]=v3.1
 ########################################################################################
 # Pipeline 1, for regions with implementation < v2024.0f, i.e. v2024.0d westernUS.
 ########################################################################################
-
-pipeLineScriptIds1=(mod09gaI spiFillC spiSmooC moSpires scdInCub daNetCDF daMosBig daGeoBig daStatis webExpSn ftpExpor)
+pipeLineBigRegionId1=5 # westernUS.
+pipeLineVersionOfAncillary1=v3.1
+pipeLineInputProductAndVersion1=mod09ga.061
+pipeLineControlScriptId1=stnr2410
+pipeLineControlTime1=11:30:00
+pipeLineScriptIds1=(mod09gaI spiFillC spiSmooC moSpires scdInCub daNetCDF daMosBig daGeoBig daStatis ftpExpor webExpSn)
 pipeLineLabels1=(v061 v2024.0d v2024.0d v2024.0d v2024.0d v2024.1.0 v2024.0d v2024.0d v2024.0d v2024.0d v2024.0d)
-pipeLineRegionTypes1=(0 0 0 0 0 0 1 1 1 10 10)
+pipeLineRegionTypes1=(0 0 0 0 0 0 1 1 1 1 10)
   # 0: tile, 1: big region, 10: all regions.
 pipeLineSequences1=(0 0 001-036 0 0 0 0 0 001-033 0 0)
 pipeLineSequenceMultiplierToIndices1=(1 1 1 1 1 1 1 1 3 1 1)
@@ -54,7 +94,7 @@ pipeLineParallelWorkersNb1=(0 18 10 10 0 2 6 0 0 0 0)
 
 # sbatch parameters
 pipeLineTasksPerNode1=(1 18 10 10 5 2 6 1 1 1 1)
-pipeLineMems1=(1G 140G 30G 40G 30G 5G 30G 8G 8G 3G 1G)
+pipeLineMems1=(1G 140G 30G 40G 30G 5G 30G 8G 8G 1G 3G)
 pipeLineTimes1=(01:30:00 02:45:00 02:30:00 00:30:00 00:20:00 00:30:00 00:40:00 00:20:00 04:00:00 01:30:00 01:30:00)
 # NB: daGeoBig: time for generation of the last day only.
 # NB: daStatis: time for 3 subdivisions only.
